6056352efeaa53754ae75007ac10ff21f5d4eb51
Giovane Moreira <giovane.fernandes.moreira@gmail.com>
2020-04-27 20:07:50 -0300

    Testando primeira classe de dominio - Categoria

--- src/main/java/br/com/geotecmanutencao/resources/CategoriaResource.java
+++ src/main/java/br/com/geotecmanutencao/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package br.com.geotecmanutencao.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.geotecmanutencao.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria categoria = new Categoria(1, "Informatica");
+		Categoria categoria2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> listaCategoria = new ArrayList<>();
+		listaCategoria.add(categoria);
+		listaCategoria.add(categoria2);
+		
+		return listaCategoria;
 	}
 
 }
