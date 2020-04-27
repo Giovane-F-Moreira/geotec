e03e19a99d7b287bd810d86ac70b719337fdd2b3
Giovane Moreira <giovane.fernandes.moreira@gmail.com>
2020-04-27 16:24:19 -0300

    Projeto Criado

---
+++ src/main/java/br/com/geotecmanutencao/GeotecApplication.java
@@ -0,0 +1,13 @@
+package br.com.geotecmanutencao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeotecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeotecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/geotecmanutencao/GeotecApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.geotecmanutencao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeotecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
